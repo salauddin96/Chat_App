b1efe8c037638010d8548844642300fae15e7284
salauddin96 <salauddin33-5088@diu.edu.bd>
2023-10-25 13:31:09 +0600

    update design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,4 +69,7 @@ dependencies {
 
     //StoryView
     implementation ("com.github.OMARIHAMZA:StoryView:1.0.2-alpha")
+
+    //Circular image view
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/org/techtales/pakhi/SetupProfileActivity.kt
+++ app/src/main/java/org/techtales/pakhi/SetupProfileActivity.kt
@@ -1,11 +1,142 @@
 package org.techtales.pakhi
 
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import org.techtales.pakhi.databinding.ActivitySetupProfileBinding
+import org.techtales.pakhi.model.User
+import java.util.Date
 
 class SetupProfileActivity : AppCompatActivity() {
+
+    var auth: FirebaseAuth? = null
+    var database: FirebaseDatabase? = null
+    var storage: FirebaseStorage? = null
+    var selectedImage: Uri? = null
+    var dialog: ProgressDialog? = null
+    private lateinit var binding: ActivitySetupProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivitySetupProfileBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setup_profile)
+        setContentView(binding.root)
+
+        dialog = ProgressDialog(this)
+        dialog!!.setMessage("Updating Profile...")
+        dialog!!.setCancelable(false)
+
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        binding.profileImage.setOnClickListener {
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image"
+            startActivityForResult(intent, 45)
+        }
+
+        binding.setupBtn.setOnClickListener {
+
+            val name: String = binding.nameETxt.text.toString()
+            if (name.isEmpty()) {
+                binding.nameETxt.setError("Please type a name...")
+
+            }
+            dialog!!.show()
+            if (selectedImage != null) {
+
+                val reference = storage!!.reference.child("Profile")
+                    .child(auth!!.uid!!)
+                reference.putFile(selectedImage!!).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val imageUrl = it.toString()
+                        val uid = auth!!.uid
+                        val phone = auth!!.currentUser!!.phoneNumber
+                        val name: String = binding.nameETxt.toString()
+                        val user = User(uid!!, name, phone!!, imageUrl)
+
+                        database!!.reference
+                            .child("users")
+                            .child(uid)
+                            .setValue(user)
+                            .addOnCompleteListener {
+
+                                dialog!!.dismiss()
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                    } else {
+
+                        val uid = auth!!.uid
+                        val phone = auth!!.currentUser!!.phoneNumber
+                        val name: String = binding.nameETxt.toString()
+                        val user = User(uid!!, name, phone!!, "No Image")
+
+                        database!!.reference
+                            .child("users")
+                            .child(uid)
+                            .setValue(user)
+                            .addOnCompleteListener {
+
+                                dialog!!.dismiss()
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data!= null) {
+
+            if (data.data!= null) {
+
+                val uri = data.data
+                val storage = FirebaseStorage.getInstance()
+                val time = Date().time
+                val reference = storage.reference
+                    .child("Profile")
+                    .child(time.toString()+"")
+                reference.putFile(uri!!).addOnCompleteListener {
+
+                    if (it.isSuccessful) {
+
+                        reference.downloadUrl.addOnCompleteListener {
+
+                            val filePath = it.toString()
+                            val obj = HashMap<String, Any>()
+                            obj["image"] = filePath
+                            database!!.reference
+                                .child("users")
+                                .child(FirebaseAuth.getInstance().uid!!)
+                                .updateChildren(obj).addOnSuccessListener {  }
+
+                        }
+
+
+                    }
+                }
+
+                binding.profileImage.setImageURI(data.data)
+                selectedImage = data.data
+            }
+        }
     }
 }
